1d79515595842b23c022e7e7b21f70191aa25170
Ayahel Omictin <163622726+ayahel13@users.noreply.github.com>
2024-04-13 07:48:53 +0800

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -7,35 +7,70 @@ export const store = new Vuex.Store({
   name: "store",
   state: {
     books: [
-      { id: 1, title: "Berserk", author: "Kentaro Miura", price: 10.01 },
-      { id: 2, title: "One Piece", author: "Eiichiro Oda", price: 12.01 },
-      { id: 3, title: "Naruto", author: "Masashi Kishimoto", price: 11.01 },
-      { id: 4, title: "Dragon Ball", author: "Akira Toriyama", price: 9.01 },
+      { 
+        id: 1, title: "Berserk", 
+        author: "Kentaro Miura",
+        price: 10.01, 
+        imageUrl: 'img/book1.jpg' },
+
+      { 
+        id: 2, title: "One Piece",
+        author: "Eiichiro Oda",
+        price: 12.01,
+        imageUrl: 'img/book2.jpg'
+       },
+      { 
+        id: 3, title: "Naruto",
+        author: "Masashi Kishimoto",
+        price: 11.01,
+        imageUrl: 'img/book3.jpg'
+      },
+      { 
+        id: 4, 
+        title: "Dragon Ball", 
+        author: "Akira Toriyama", 
+        price: 9.01,
+        imageUrl: 'img/book4.jpg'
+      },
       {
         id: 5,
         title: "Attack on Titan",
         author: "Hajime Isayama",
         price: 13.01,
+        imageUrl: 'img/book5.jpg'
       },
       {
         id: 6,
         title: "My Hero Academia",
         author: "Kohei Horikoshi",
         price: 10.99,
+        imageUrl: 'img/book6.jpg'
       },
-      { id: 7, title: "Death Note", author: "Tsugumi Ohba", price: 11.5 },
+      { 
+        id: 7, title: "Death Note",
+        author: "Tsugumi Ohba", 
+        price: 11.5,
+        imageUrl: 'img/book7.jpg'
+       },
       {
         id: 8,
         title: "Fullmetal Alchemist",
         author: "Hiromu Arakawa",
         price: 12.75,
+        imageUrl: 'img/book8.jpg'
       },
-      { id: 9, title: "Tokyo Ghoul", author: "Sui Ishida", price: 9.75 },
+      { id: 9, 
+        title: "Tokyo Ghoul", 
+        author: "Sui Ishida", 
+        price: 9.75,
+        imageUrl: 'img/book9.jpg'
+      },
       {
         id: 10,
         title: "Demon Slayer",
         author: "Koyoharu Gotouge",
         price: 13.99,
+        imageUrl: 'img/book10.jpg'
       },
     ],
     cart: [],
@@ -113,3 +148,4 @@ export const store = new Vuex.Store({
     },
   },
 });
+
